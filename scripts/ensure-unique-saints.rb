e7a54a3bec853b910b132d48fdc92f6487abd520
jsdnhk <dn@jsdnhk.com>
2020-02-21 03:08:13 +0800

    updated links+quotes with latest date

---
+++ scripts/ensure-unique-saints.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Ensures that quote texts in json files(quote) are appeared once only
+
+require 'json'
+
+saints_folder_path = File.expand_path('../assets/saints', File.dirname(__FILE__))
+json_files = Dir["#{saints_folder_path}/*.json"]
+
+class String
+  def formatted
+    !(self.end_with?('.') || self.end_with?('?') || self.end_with?('!')) ? self.strip + "." : self.strip
+  end
+end
+
+is_success = true
+begin
+  json_files.each do |json_file|
+    data = JSON.load(File.read(json_file))
+    data_results_new = Array.new(data['results'])
+    data_quotes = []
+    data_results_new.reject! do |result|
+      result.delete("_id")
+      if data_quotes.any? { |quote| quote.formatted.eql?(result["quoteText"].formatted) }
+        true
+      else
+        data_quotes.append(result["quoteText"].strip)
+        false
+      end
+    end
+    next if(!data_results_new || data_results_new.empty?)
+    data = {count: data_results_new.length, results: data_results_new}
+    File.open(json_file, "w") { |f| f.write(JSON.pretty_generate(data)) }
+  end
+rescue
+  $stderr.puts "Error occurred: #{$!}"
+  is_success = false
+ensure
+  $stdout.puts( is_success ? "Success!" : "Failed" )
+  exit(is_success)
+end
---
+++ scripts/get-pages-latest-date.rb
@@ -0,0 +1,52 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Get all the pages latest updated dates and export the result to yaml file
+
+require 'yaml'
+
+# Dir["**/*"], to fetch inside the directories as well
+pages_folder_path = File.expand_path('../pages', File.dirname(__FILE__))
+data_folder_path = File.expand_path('../_data', File.dirname(__FILE__))
+collections_folder_path = File.expand_path('../collections', File.dirname(__FILE__))
+pages = Dir["#{pages_folder_path}/*"].map { |dir| File.basename(dir.to_s.strip) }
+
+latest_dates = []
+output_yaml = "#{data_folder_path}/pages_latest.yaml"
+
+def getLatestTimeFiles(arr_files)
+  return nil if(!arr_files || arr_files.empty?)
+  mtime_latest = nil
+  arr_files.each do |file|
+    if mtime_latest
+      mtime_latest = File.mtime(file) if File.mtime(file) > mtime_latest
+    else
+      mtime_latest = File.mtime(file)
+    end
+  end
+  mtime_latest
+end
+
+pages.each do |page|
+  arr_page_files =
+  case page
+  when 'blog'
+    Dir["#{collections_folder_path}/_posts/*"]
+  when 'debug'
+    Dir["#{pages_folder_path}/debug/*"]
+  when 'links'
+    Dir["#{data_folder_path}/links.yaml"]
+  when 'quotes'
+    Dir["#{data_folder_path}/quotes/*"]
+  when 'resume'
+    Dir["#{pages_folder_path}/resume/*"]
+  else
+    raise("have page which is not yet handled.[#{page}]")
+  end
+  mtime_page = getLatestTimeFiles(arr_page_files)
+  latest_date = {name: page, mtime: (mtime_page ? mtime_page.strftime("%Y%m%d") : nil)}
+  latest_dates.append(latest_date)
+end
+File.write(output_yaml, latest_dates.to_yaml)
+
+
